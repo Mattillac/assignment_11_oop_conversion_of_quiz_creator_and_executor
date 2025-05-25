0cbd5a5c111c960da718aa0fd436a625a73917f3
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-05-25 18:19:04 +0800

    here is my pseudocode for my quiz executor and i hope i met my own criteria as well

---
+++ quiz_executor.py
@@ -0,0 +1,5 @@
+#class here again
+#since i have different parts and it was defined already so it'll be esay to be organized.
+#define here 
+#source code
+#and other files when creating class and the main engine as well.
