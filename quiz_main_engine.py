12d62258bc5846947f66bcdb9f8f446db7818a8a
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-05-25 19:25:59 +0800

    now here is my main engine as you can see that  it will now put all the parts together in order to process this program.

--- quiz_main_engine.py
+++ quiz_main_engine.py
@@ -1,1 +1,15 @@
+from quiz_loader import QuizLoader
+from quiz_executor import Quiz
+from ascii_art import AsciiArt
+
+def main():
+    filename = "quiz.txt"
+    questions = QuizLoader.load_quiz(filename)
+    quiz = Quiz(questions)
+    quiz.run()
+    quiz.show_results()
+    AsciiArt.thank_you()
+
+if __name__ == "__main__":
+    main()
 
