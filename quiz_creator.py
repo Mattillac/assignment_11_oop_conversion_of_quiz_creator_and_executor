2f667b44ef117937a817b70030707273b7651445
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-05-25 17:08:10 +0800

    here is my creating class for my first half of the code and i only started at the quiz name searcher and the question and answers creators.  as i followed the previous lesson and applied it on my program. thanks to sir!

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,21 @@
 #class here
+class QuizCreator:
 #some def in here or using imports
 #source code goes here
+def __init__(self):
+        self.quiz_name = ""
+        self.file = None
+
+    def get_quiz_name(self):
+        self.quiz_name = input("Enter the quiz name: ")
+        if not self.quiz_name.endswith(".txt"):
+            self.quiz_name += ".txt"
+        self.file = open(self.quiz_name, "a")
+
+    def add_question(self):
+        question = input("Enter a question: ")
+        answer_sheet = input("Enter the answer sheet like this (a, b, c, d (a) ) and enclose with parenthesis the correct answer!: ")
+        self.file.write("Question: " + question + "\n")
+        self.file.write("Answers and Correct Answer: " + answer_sheet + "\n\n")
+
 #and create a different file for the main function
